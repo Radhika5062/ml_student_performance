33b5e4a07597a5d6b435059379bf58801b697aee
Radhika5062 <radhikamaheshwari26@gmail.com>
2024-09-10 22:12:47 +1000

    hyperparameter tuning added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -33,8 +33,8 @@ class ModelTrainer:
                 test_array[:,-1]
             )
             models = {
-                "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbours": KNeighborsRegressor(),
@@ -43,7 +43,47 @@ class ModelTrainer:
                 "Adaboost":AdaBoostRegressor()
             }
 
-            model_report:dict = evaluate_models(X_train=X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models)
+            params = {
+                "Decision Tree":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter': ['best', 'random'],
+                    "max_features": ['sqrt', 'log2']
+                },
+                "Random Forest": {
+                    'criterion':['squared_error', 'absolute_error', 'friedman_mse', 'poisson'],
+                    "max_features": ['sqrt', 'log2'],
+                    "n_estimators":[8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting":{
+                    'loss': ['squared_error', 'absolute_error', 'huber', 'quantile'],
+                    'learning_rate':[0.1, 0.01, 0.05, 0.0001],
+                    'subsample':[0.6, 0.7, 0.75, 0.8, 0.9],
+                    'criterion':['friedman_mse', 'squared_error'],
+                    "max_features": ['sqrt', 'log2'],
+                    "n_estimators":[8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "K-Neighbours": {
+                    "weights": ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "XGBoost": {
+                    'learning_rate':[0.1, 0.01, 0.05, 0.0001],
+                    "n_estimators":[8, 16, 32, 64, 128, 256]
+                },
+                "Catboost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report:dict = evaluate_models(X_train=X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models, params = params)
 
             # to get the best model from dictionary
             best_model_score = max(sorted(model_report.values()))
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from exception import CustomException
 from logger import logging
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,12 +18,28 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(models)):
+            logging.info(f"Models = {models}")
             model = list(models.values())[i]
+            logging.info(f"Model = {model}")
+            logging.info(f"Model = {list(models.keys())[i]}")
+            param = params[list(models.keys())[i]]
+            logging.info(f"Param = {param}")
+
+            logging.info(f"Currently evaluating {model}")
+            logging.info(f"Current Params are {param}")
+
+            gs = GridSearchCV(model, param, cv = 3)
+            gs.fit(X_train, y_train)
+
+            logging.info(f'Best params are {gs.best_params_}')
+
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train, y_train) # Training
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -31,6 +48,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
+            logging.info(f'report = {report}')
         return report
     except Exception as e:
         raise CustomException(e, sys)
